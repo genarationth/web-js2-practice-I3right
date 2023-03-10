6e551c1eb8d6b87ffd30618b0f8806e48b4a94b8
Bright <108338051+I3right@users.noreply.github.com>
2023-03-10 22:31:38 +0700

    Add show both result

--- script_example2.js
+++ script_example2.js
@@ -3,11 +3,11 @@
 let userBirthYear = 1997;
 let futureYear = 2030;
 let possibleAge = futureYear - userBirthYear;
-console.log(possibleAge);
+console.log(possibleAge); // resuld = 33
 
 // Part2 : Challenge 2
 let userName = "Bright";
 let userLocation = "CNX";
 let userHobby = "Cycling";
 let card = `Hi, my name is ${userName}. I live in ${userLocation} and enjoy ${userHobby}`;
-console.log(card);
+console.log(card); // result = Hi, my name is Bright. I live in CNX and enjoy Cycling
