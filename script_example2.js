7d488afc24b5389d039da9f7553e9ef50f90998c
Bright <108338051+I3right@users.noreply.github.com>
2023-03-10 22:30:26 +0700

    Done homework part 2 Both challenge

---
+++ script_example2.js
@@ -0,0 +1,13 @@
+// Part2 : Challenge 1
+// Calculate age of user
+let userBirthYear = 1997;
+let futureYear = 2030;
+let possibleAge = futureYear - userBirthYear;
+console.log(possibleAge);
+
+// Part2 : Challenge 2
+let userName = "Bright";
+let userLocation = "CNX";
+let userHobby = "Cycling";
+let card = `Hi, my name is ${userName}. I live in ${userLocation} and enjoy ${userHobby}`;
+console.log(card);
